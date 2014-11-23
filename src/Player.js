641686309c44f6c425b9ccaa59cc539bc1782137
Raicuparta <raicuparta@gmail.com>
2014-11-23 19:39:33 +0000

    player

--- src/Player.js
+++ src/Player.js
@@ -12,7 +12,13 @@ Player.prototype = {
 	},
 
 	create: function() {
+		game.camera.follow(_spriteMain);
+		
 		spriteMain = game.add.sprite(10, 10, 'spriteMain');
+		game.physics.enable(spriteMain, Phaser.Physics.ARCADE);
+		spriteMain.anchor.setTo(.5,.5);
+		spriteMain.body.collideWorldBounds = true;
+        spriteMain.body.setSize(14, 50, 0, 6);
 	},
 
 	update: function() {
