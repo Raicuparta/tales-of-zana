fd9d47ea99097637cd90a4d7bf492d025e1fdfb8
Blamepie <tiago_j.s@hotmail.com>
2014-11-23 19:36:17 +0000

    putting the Block in the other basket

--- src/Block.js
+++ src/Block.js
@@ -1,19 +1,17 @@
-Block = function(game) {
+Block = function(g) {
 
-	this.game = game;
-	this.sprite = null,
-	this.active = false,
-	this.soundStepGrass = null
+	game = g;
+	sprite = null;
 };
 
 Block.prototype = {
 
 	preload: function() {
-    	this.game.load.spritesheet('sprite', 'assets/sprites/block.png', 52, 50);
+    	game.load.spritesheet('sprite', 'assets/sprites/block.png', 52, 50);
 	},
 
 	create: function(layer1) {
-
+		sprite = game.add.sprite(2170, 640, 'sprite');
 	},
 
 	update: function() {
