384b5efcf208650e62834101e6f2c4fbaf1d5c4a
Raicuparta <raicuparta@gmail.com>
2014-11-23 19:29:31 +0000

    putting the player in the basket

--- src/GameMananger.js
+++ src/GameMananger.js
@@ -2,6 +2,7 @@ GameMananger = function(g) {
 
 	game = g;
 	level = null;
+	player = null;
 
 };
 
@@ -10,14 +11,19 @@ GameMananger.prototype = {
 	preload: function() {
     	level = new Level(game);
 		level.preload();
+
+		player = new Player(game);
+		player.preload();
 	},
 
 	create: function() {
 		level.create();
+		player.create();
 	},
 
 	update: function() {
 		level.update();
+		player.update();
 	}
 	
 };
--- src/Player.js
+++ src/Player.js
@@ -1,16 +1,18 @@
-Player = function(game) {
+Player = function(g) {
 
-	this.game = game;
+	game = g;
+	spriteMain = null;
+
 };
 
 Player.prototype = {
 
 	preload: function() {
-    	
+    	game.load.spritesheet('spriteMain', 'assets/sprites/playerMain.png', 62, 62);
 	},
 
 	create: function() {
-		
+		spriteMain = game.add.sprite(10, 10, 'spriteMain');
 	},
 
 	update: function() {
